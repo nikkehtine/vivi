3c8369b346e9a1dc6f69869d64d20ada2bd14c30
nikkehtine <nikkehtine@int.pl>
2024-10-26 19:10:07 +0200

    Add visual representation for duration and a callback

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <raylib.h>
 #include <stdio.h>
 
+void callback(void *bufferData, unsigned int frames) {
+  (void)bufferData;
+  (void)frames;
+}
+
 int main(int argc, char **argv) {
   const int w = 800;
   const int h = 450;
@@ -13,9 +18,19 @@ int main(int argc, char **argv) {
   PlayMusicStream(music);
   SetMusicVolume(music, 0.5f);
 
+  AttachAudioStreamProcessor(music.stream, callback);
+
+  float time_played = 0.0f;
+  float time_length = 0.0f;
+  float time_duration = 0.0f;
+  char time_played_str[8];
+  char time_length_str[8];
+  char time_duration_str[8];
+
   while (!WindowShouldClose()) {
     UpdateMusicStream(music);
 
+    // Get user input
     if (IsKeyPressed(KEY_SPACE)) {
       if (IsMusicStreamPlaying(music))
         PauseMusicStream(music);
@@ -23,11 +38,25 @@ int main(int argc, char **argv) {
         ResumeMusicStream(music);
     }
 
+    // Update the counters
+    time_played = GetMusicTimePlayed(music);
+    time_length = GetMusicTimeLength(music);
+    time_duration = time_played / time_length;
+    if (time_duration > 1.0f)
+      time_duration = 1.0f;
+
+    sprintf(time_played_str, "%.2f", time_played);
+    sprintf(time_length_str, "%.2f", time_length);
+    sprintf(time_duration_str, "%.2f", time_duration);
+
     // Draw
     BeginDrawing();
 
-    ClearBackground(RED);
-    DrawText("Hello, world!", 350, 200, 20, BLACK);
+    ClearBackground(BLACK);
+
+    DrawText(time_played_str, 380, 170, 20, RAYWHITE);
+    DrawText(time_length_str, 380, 200, 20, RAYWHITE);
+    DrawText(time_duration_str, 380, 230, 20, RAYWHITE);
 
     EndDrawing();
   }
